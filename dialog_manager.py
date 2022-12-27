76a4dd0621bdf606926e2915e6cbae2cd283e058
CreativeeBlackWolf <32525436+CreativeeBlackWolf@users.noreply.github.com>
2022-12-27 08:07:01 +0300

    инвентарь command for admins

---
+++ dialog_manager.py
@@ -0,0 +1,34 @@
+import discord
+import player
+from multipledispatch import dispatch
+
+@dispatch(discord.Message, player.Player)
+async def send_inventory(message: discord.Message, player: player.Player) -> None:
+    """
+    send 
+    """
+    inv = "\n".join(player.inventory)
+    await message.channel.send(f'''```ansi
+Инвентарь:
+{inv}
+```''')
+
+@dispatch(discord.Message, list)
+async def send_inventory(message: discord.Message, inventory: list, format = True) -> None:
+    inv = "\n".join(player.Player.format_inventory(inventory) if format else inventory)
+    await message.delete()
+    await message.channel.send(f'''```ansi
+Инвентарь:
+{inv}
+```''')
+
+async def send_abilities(message: discord.Message, player: player.Player) -> None:
+    active = "\n".join(player.active_abilities)
+    passive = "\n".join(player.passive_abilities)
+    await message.channel.send(f'''```
+Навыки:
+{active}
+
+Особенности:
+{passive}
+```''')
