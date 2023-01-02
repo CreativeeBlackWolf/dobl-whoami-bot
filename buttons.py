dae33f969bf9c4133040c96b3011b986137fe33d
CreativeeBlackWolf <32525436+CreativeeBlackWolf@users.noreply.github.com>
2023-01-02 04:04:14 +0300

    add где я to buttons

--- buttons.py
+++ buttons.py
@@ -9,31 +9,45 @@ import player
 
 
 class WhoamiCommandView(View):
-    def __init__(self, map: mapparser.Map, player: player.Player):
+    def __init__(self, map: mapparser.Map, player: player.Player, author: discord.User):
         super().__init__(timeout=10.0)
         self.map = map
         self.player = player
+        self.author = author
 
     @button(label="Персонаж", 
             custom_id="player", 
             style=ButtonStyle.blurple, 
             emoji=random.choice(["🤔", "😐", "🤡"]))
     async def player_button_callback(self, interaction: discord.Interaction, button: Button):
-        for childButton in self.children:
-            childButton.style = ButtonStyle.green if childButton != button else ButtonStyle.blurple
-        await interaction.response.edit_message(view=self, content=dialog.get_player_info(self.map, self.player))
+        if interaction.user.id == self.author.id:    
+            for childButton in self.children:
+                childButton.style = ButtonStyle.green if childButton != button else ButtonStyle.blurple
+            await interaction.response.edit_message(view=self, content=dialog.get_player_info(self.map, self.player))
 
     @button(label="Инвентарь", custom_id="inventory", style=ButtonStyle.success, emoji="📦")
     async def inventory_button_callback(self, interaction: discord.Interaction, button: Button):
-        for childButton in self.children:
-            childButton.style = ButtonStyle.green if childButton != button else ButtonStyle.blurple
-        await interaction.response.edit_message(view=self, content=dialog.get_inventory_string(self.player))
+        if interaction.user.id == self.author.id:
+            for childButton in self.children:
+                childButton.style = ButtonStyle.green if childButton != button else ButtonStyle.blurple
+            await interaction.response.edit_message(view=self, content=dialog.get_inventory_string(self.player))
 
     @button(label="Навыки", custom_id="abilities", style=ButtonStyle.success, emoji="🔶")
     async def abilities_button_callback(self, interaction: discord.Interaction, button: Button):
-        for childButton in self.children:
-            childButton.style = ButtonStyle.green if childButton != button else ButtonStyle.blurple
-        await interaction.response.edit_message(view=self, content=dialog.get_abilities_string(self.player))
+        if interaction.user.id == self.author.id:
+            for childButton in self.children:
+                childButton.style = ButtonStyle.green if childButton != button else ButtonStyle.blurple
+            await interaction.response.edit_message(view=self, content=dialog.get_abilities_string(self.player))
+
+    @button(label="Где я", custom_id="whereami", style=ButtonStyle.success, emoji="🗺️")
+    async def whereami_button_callback(self, interaction: discord.Interaction, button: Button):
+        if interaction.user.id == self.author.id:
+            for childButton in self.children:
+                childButton.style = ButtonStyle.green if childButton != button else ButtonStyle.blurple
+            await interaction.response.edit_message(view=self, content=
+            f"""```ansi
+{self.map.construct_ascii_repr(self.player)}
+```""")
 
     async def on_timeout(self) -> None:
         for button in self.children:
